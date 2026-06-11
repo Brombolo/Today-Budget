23ab71372ebe2cacdf03466266c324175b576f81
Brombolo <polloblu00@gmail.com>
2026-06-11 17:47:11 +0200

    Update minSdk to 29 and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
   defaultConfig {
     applicationId = "com.aistudio.todaybudget.bswzqp"
-    minSdk = 24
+    minSdk = 29
     targetSdk = 36
     versionCode = 1
     versionName = "1.0.0-beta"
